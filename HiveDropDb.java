bb7308871aeacc1e3e1aa1cf4160cb6d8885fe8b
ChaeYoon Kim <kstar951@naver.com>
2017-09-06 16:51:51 +0900

    Create HiveDropDb.java

---
+++ HiveDropDb.java
@@ -0,0 +1,24 @@
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.DriverManager;
+
+public class HiveDropDb {
+   private static String driverName = "org.apache.hadoop.hive.jdbc.HiveDriver";
+   
+   public static void main(String[] args) throws SQLException {
+   
+      // Register driver and create driver instance
+      Class.forName(driverName);
+      
+      // get connection
+      Connection con = DriverManager.getConnection("jdbc:hive://localhost:10000/default", "", "");
+      Statement stmt = con.createStatement();
+      stmt.executeQuery("DROP DATABASE userdb");
+      
+      System.out.println(“Drop userdb database successful.”);
+      
+      con.close();
+   }
+}
